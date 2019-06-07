13ca4b23744bf525661e109f66f221356d40c6cd
Christophe Irles <christophe.irles@al-enterprise.com>
2019-06-07 17:58:21 +0200

    Add missing files

---
+++ Extensions/XEP-configuration/RoomInvitationEventArgs.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Sharp.Xmpp.Extensions
+{
+    public class RoomInvitationEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The room  id 
+        /// </summary>
+        public String RoomId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The room jid
+        /// </summary>
+        public String RoomJid
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The roon name
+        /// </summary>
+        public String RoomName
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// The User Id who send the invitation
+        /// </summary>
+        public String UserId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The User Jid who send the invitation
+        /// </summary>
+        public String UserJid
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The User Display Name who send the invitation
+        /// </summary>
+        public String UserDisplayName
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The subject's invitation
+        /// </summary>
+        public String Subject
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the FavoriteManagementEventArgs class.
+        /// </summary>
+        /// <param name="id">the favorite id</param>
+        /// <param name="action">the action about this favorite</param>
+        /// <param name="type">the favorite's type</param>
+        /// <param name="position">the favorite's status</param>
+        /// <param name="peerId">the favorite's peerId</param>
+        /// <summary>
+        public RoomInvitationEventArgs(String roomId, String roomJid, String roomName, String userId, String userJid, String userDisplayName, String subject)
+        {
+            RoomId = roomId;
+            RoomJid = roomJid;
+            RoomName = roomName;
+            UserId = userId;
+            UserJid = userJid;
+            UserDisplayName = userDisplayName;
+            Subject = subject;
+        }
+    }
+}
---
+++ Extensions/XEP-configuration/RoomManagementEventArgs.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Sharp.Xmpp.Extensions
+{
+    public class RoomManagementEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The room  id 
+        /// </summary>
+        public String RoomId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The room jid
+        /// </summary>
+        public String RoomJid
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// UserJid (if any) accepted / invited / unsubscribed / deleted
+        /// </summary>
+        public String UserJid
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// User status (if any) accepted / invited / unsubscribed / deleted
+        /// </summary>
+        public String Status
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Room's updated name (if any)
+        /// </summary>
+        public String Name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Room's updated topic (if any)
+        /// </summary>
+        public String Topic
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Last avatar update date (if any)
+        /// </summary>
+        public String LastAvatarUpdateDate
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the RoomManagementEventArgs class.
+        /// </summary>
+        /// <param name="roomId">the room id</param>
+        /// <param name="roomJid">the room jid</param>
+        /// <param name="userJid">the user JID</param>
+        /// <param name="status">user status</param>
+        /// <param name="name">the room name</param>
+        /// <param name="topic">the room topic</param>
+        /// <param name="lastAvatarUpdateDate">the last avatar romm update date</param>
+        /// <summary>
+        public RoomManagementEventArgs(String roomId, String roomJid, String userJid, String status, String name, String topic, String lastAvatarUpdateDate)
+        {
+            RoomId = roomId;
+            RoomJid = roomJid;
+            UserJid = userJid;
+            Status = status;
+            Name = name;
+            Topic = topic;
+            LastAvatarUpdateDate = lastAvatarUpdateDate;
+        }
+    }
+}
