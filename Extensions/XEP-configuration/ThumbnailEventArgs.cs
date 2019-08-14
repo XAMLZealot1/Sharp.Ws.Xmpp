a2dec473a921236a38a301ccc0922b23f9ac0835
Christophe Irles <christophe.irles@al-enterprise.com>
2019-08-14 11:34:03 +0200

    Include missing files

---
+++ Extensions/XEP-configuration/ThumbnailEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sharp.Xmpp.Extensions
+{
+    public class ThumbnailEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The file id 
+        /// </summary>
+        public String FileId { get; private set; }
+
+        /// <summary>
+        /// Witdh of the image file 
+        /// </summary>
+        public int Width{ get; private set; }
+
+        /// <summary>
+        /// Height of the image file 
+        /// </summary>
+        public int Height { get; private set; }
+
+        public ThumbnailEventArgs(String fileId, int width, int height)
+        {
+            FileId = fileId;
+            Width = width;
+            Height = height;
+        }
+    }
+}
