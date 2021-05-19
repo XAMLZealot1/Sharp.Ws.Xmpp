5b6b241a9632b2f4e100864ac028c6d3943b41af
Christophe Irles <christophe.irles@al-enterprise.com>
2021-05-19 15:03:07 +0200

    Set KeepAliveInterval to 2 minutes

--- Core/WebSocket.cs
+++ Core/WebSocket.cs
@@ -112,7 +112,7 @@ namespace Sharp.Xmpp.Core
             // Create Client
             clientWebSocket = new ClientWebSocket();
 
-            clientWebSocket.Options.KeepAliveInterval = TimeSpan.FromSeconds(2); ;
+            clientWebSocket.Options.KeepAliveInterval = TimeSpan.FromMinutes(2);
 
 #if NETCOREAPP
             clientWebSocket.Options.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => {
